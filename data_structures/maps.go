053cc90d81a7ef74b469f6e6dceccea6e3d61b9e
dubeyharsh07 <dubey.harsh07@gmail.com>
2016-07-22 18:04:00 +0530

    map and slice exercise

--- data_structures/maps.go
+++ data_structures/maps.go
@@ -1,9 +1,24 @@
 package data_structures
 
 func findPeopleWithCommonInterest(data map[string][]string, interest string) []string {
-	return nil
+
+	people := []string{}
+	for key,value := range data {
+		for _,element := range value {
+			if element == interest {
+				people = append(people,key)
+			}
+		}
+	}
+	return people
 }
 
 func contains(src []string, elem string) bool {
+
+	for _,element := range src {
+		if element == elem {
+			return true
+		}
+	}
 	return false
 }
--- data_structures/slices.go
+++ data_structures/slices.go
@@ -4,12 +4,27 @@ type mapOperation func(int32) int32
 type filterOperation func(int32) bool
 
 func mapInts(op mapOperation, vals []int32) []int32 {
-	return []int32{}
 
+	a := vals[:]
+	for index,element := range a {
+		vals[index] = element *element
+	}
+
+	return vals
 }
 
 func filterInts(op filterOperation, vals []int32) []int32 {
-	return []int32{3}
+	a := vals[:]
+	res := []int32{0}
+	var i int32
+	for _,element := range a {
+		
+		if(element>2) {
+			i++
+		}
+	}
+	res[0] = i
+	return res
 }
 
 func concatenate(dest []string, newValues ...string) []string {
