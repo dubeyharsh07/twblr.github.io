398349e349a3822d0d76e8091f5a10cb9f17df32
dubeyharsh07 <dubey.harsh07@gmail.com>
2016-07-22 16:08:59 +0530

    changed for structure

--- control_structures/for_structure.go
+++ control_structures/for_structure.go
@@ -1,5 +1,23 @@
 package control_structures
 
 func collatzChainLength(n int) int {
-	return 0
+
+	if n%2 != 0 {
+		n = 3*n + 1 
+	}
+
+	i := 1;
+	for n>1 {
+		
+
+		if n%2 != 0 {
+		n = 3*n + 1 
+		}else {
+			n = n/2;
+		}
+
+		i++;
+
+	}
+	return i
 }
