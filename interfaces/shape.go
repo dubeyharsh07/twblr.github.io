90c35d0076453db0f3a175f9ad264fcf5374eeef
dubeyharsh07 <dubey.harsh07@gmail.com>
2016-07-23 10:50:01 +0530

    Shape interface code

--- interfaces/shape.go
+++ interfaces/shape.go
@@ -3,3 +3,28 @@ package interfaces
 type Shape interface {
 	Area() int
 }
+
+type Rectangle struct {
+	length,breadth int
+}
+
+type Square struct {
+	side int
+}
+
+type Hybrid struct {
+	square Shape
+	rectangle Shape
+}
+
+func (r Rectangle) Area() int {
+	return r.length * r.breadth
+}
+
+func (square Square) Area() int {
+	return square.side * square.side
+}
+
+func (h Hybrid) Area() int {
+	return h.square.Area() + h.rectangle.Area()
+}
